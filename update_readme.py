6916d6d54cb27fef8451e1767c40ac4ca3d7f5e4
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-11-26 20:44:29 +0545

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -6,9 +6,12 @@ markdown_files = [file for file in os.listdir('.') if file.endswith('.md')]
 # Open README.md file to write the consolidated content
 with open('README.md', 'w') as readme:
     for file_name in markdown_files:
-        with open(file_name, 'r') as md_file:
-            content = md_file.read()
-            # Write content to README.md
-            readme.write(f"## {file_name}\n\n")
-            readme.write(content)
-            readme.write('\n\n---\n\n')  # Separation between files
+        if file_name == "README.md":
+            print('Skipping README.md')  # Added print statement for clarity
+        else:    
+            with open(file_name, 'r') as md_file:
+                content = md_file.read()
+                # Write content to README.md
+                readme.write(f"## {file_name}\n\n")
+                readme.write(content)
+                readme.write('\n\n---\n\n')  # Separation between files
